3cdc2c5e3691385a0c80990fd1430320e5278873
VisionR Dev2BG Team <visionr@plan-vision.com>
2016-12-25 15:29:45 +0200

    status

--- js/external-events.js
+++ js/external-events.js
@@ -1,27 +1,26 @@
+var driver = require('./ext/driver');
+
 function dbq(pgc, q, p) {
-	return new Promise((res, rej) => 
+	return new Promise((res, rej) =>
 		pgc.query(q, p, (err, data) => {
 			if (err) rej(err);
 			else res(data);
 		}));
 }
 
-var driver = require('./ext/driver');
-var dbcon = driver.connect(function(err, pgc, done) { 
-				exports.events = {
-					"client-ping": data => {
-							dbq(pgc, 'UPDATE tracking.devices SET ts_seen = now() where imei = $1', [ data.code ])
-								.then(res => { 
-									console.log("client-ping => db");
-								})
-								.catch(e => console.log(e));
-							},
-					"client-register": data => {
-							dbq(pgc, 'UPDATE tracking.devices SET ts_reg = now() where imei = $1', [ data.code ])
-								.then(res => { 
-									console.log("client-reg => db");
-								})
-								.catch(e => console.log(e));
-							}
-					};
+var dbcon = driver.connect(function (err, pgc, done) {
+	exports.events = {
+		"client-ping": data => {
+			dbq(pgc, 
+				'UPDATE tracking.devices SET ts_seen = now(), ts_serv = $1 where imei = $2', 
+				[new Date(data.time), data.code])
+				.then(res => console.log(`client-ping => ${JSON.stringify(data)}`))
+				.catch(e => console.log(e));
+		},
+		"client-register": data => {
+			dbq(pgc, 'UPDATE tracking.devices SET ts_reg = now() where imei = $1', [data.code])
+				.then(res => console.log(`client-register => ${JSON.stringify(data)}`))
+				.catch(e => console.log(e));
+		}
+	};
 });
